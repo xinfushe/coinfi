6f0018e57b9f7d21637c2740084870df54dd45ec
Voltron <cryptobotmachine@gmail.com>
2018-08-28 01:03:06 -0400

    enable night mode for toggling filter panel cancel, apply, and watchlist #796284012114656

--- app/javascript/App/components/NewsfeedPage/NewsListHeader.js
+++ app/javascript/App/components/NewsfeedPage/NewsListHeader.js
@@ -7,7 +7,7 @@ import CoinTipsTab from '../../bundles/common/components/CoinTipsTab'
 const NewsListHeader = (props) => {
   const { enableUI, currentUI, coins, feedSources, newsfeedTips } = props
 
-  const toggleFilters = () =>
+  const toggleFilterPanel = () =>
     enableUI('filterPanel', { fullScreen: window.isMobile })
 
   const showCoinListDrawer = () =>
